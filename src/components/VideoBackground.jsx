2d711cff226a5213a84bc9b0b064192b717e41d4
deveshtuteja <deveshtuteja17@gmail.com>
2024-08-07 22:40:33 +0530

    Video Background Component, movie trailers

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,5 +1,21 @@
-const VideoBackground = () => {
-  return <div>VideoBackground</div>;
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
+
+const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  useMovieTrailer(movieId);
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
--- src/utils/moviesSlice.jsx
+++ src/utils/moviesSlice.jsx
@@ -4,13 +4,17 @@ const moviesSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
+    trailerVideo: null,
   },
   reducers: {
     addNowPlayingMov: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
+    },
   },
 });
 
-export const { addNowPlayingMov } = moviesSlice.actions;
+export const { addNowPlayingMov, addTrailerVideo } = moviesSlice.actions;
 export default moviesSlice.reducer;
